776ccaec62f208de9138d7424c20de59d0227ade
clogerie <66565287+clogerie@users.noreply.github.com>
2022-06-16 17:19:14 -0400

    created Notes class with methods to create/delete notes

--- db/note.js
+++ db/note.js
@@ -1,3 +1,39 @@
 const util = require("util");
 const fs = require("fs");
+// generates unique id's
+const uuidv1 = require("uuid/v1");
+const readNote = util.promisify(fs.readFile);
+const writeNote = util.promisify(fs.watchFile);
 
+class Notes {
+    read() {
+        return readNote("db/db.json", "utf8");
+    }
+    write(note) {
+        return writeNote("db/db.json", JSON.stringify(note));
+        console.log(note);
+    }
+    getNotes() {
+        return this.read().then((notes) => {
+            let usersNotes;
+            if (usersNotes === [].concat(JSON.parse(notes))) {
+                usersNotes = []
+            } else {
+                console.log("No notes found!");
+            }
+        }); 
+    }
+    addNotes(note) {
+        const {title, text} = note;
+        if(!title || !text) {
+            console.log("Must add title and text to your note!");
+        }
+        const newNote = {title, text, id: uuidv1()};
+        return this.getNotes().then((notes) => [...notes, newNote]).then((updatedNote) => this.write(updatedNote)).then(() => newNote);
+    }
+    removeNotes(id) {
+        return this.getNotes().then((notes) => notes.filter((note) => note.id !==id)).then((filteredNote) => this.write(filteredNote));
+    }
+}
+
+module.exports = new Notes();
