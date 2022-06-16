c2e7ff0aafb72782fd8dcad99c6f7b961181baaf
clogerie <66565287+clogerie@users.noreply.github.com>
2022-06-16 17:31:37 -0400

    created route for delete request

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -7,3 +7,8 @@ router.get("/notes", (req, res) => {
 router.post("/notes", (req, res) => {
     notes.addNotes(req.body).then((note) => res.json(note)).catch((err) => res.status(500).json(err));
 });
+router.delete("/notes/:id", (req, res) => {
+    notes.removeNotes(req.params.id).then(() => res.json({ok: true})).catch((err) => res.status(500).json(err));
+});
+
+module.exports = router;
