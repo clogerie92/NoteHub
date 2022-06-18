baead0e787c8b341ae2e4508b97ea6d885bcbc7e
clogerie <66565287+clogerie@users.noreply.github.com>
2022-06-18 14:06:03 -0400

    rewrote api routes

--- db/notes.js
+++ db/notes.js
@@ -1,47 +1,47 @@
-const util = require("util");
-const fs = require("fs");
-// generates unique id's
-// const uuidv1 = require("uuid/v1");
-const { v1: uuidv1 } = require('uuid');
-const readNote = util.promisify(fs.readFile);
-const writeNote = util.promisify(fs.writeFile);
+// const util = require("util");
+// const fs = require("fs");
+// // generates unique id's
+// // const uuidv1 = require("uuid/v1");
+// const { v1: uuidv1 } = require('uuid');
+// const readNote = util.promisify(fs.readFile);
+// const writeNote = util.promisify(fs.writeFile);
 
-class Notes {
-    read() {
-        return readNote("db/db.json", "utf8");
-    }
-    write(note) {
-        return writeNote("db/db.json", JSON.stringify(note));
-        console.log(note);
-    }
-    getNotes() {
-        return this.read().then((notes) => {
-            let usersNotes;
-            if (usersNotes = [].concat(JSON.parse(notes))) {
-                usersNotes = [];
-            } else {
-                console.log("No notes found!");
-            }
-            // try {
-            //     usersNotes === [].concat(JSON.parse(notes));
-            // } catch (err) {
-            //     usersNotes = [];
-            // }
+// class Notes {
+//     read() {
+//         return readNote("db/db.json", "utf8");
+//     }
+//     write(note) {
+//         return writeNote("db/db.json", JSON.stringify(note));
+//         console.log(note);
+//     }
+//     getNotes() {
+//         return this.read().then((notes) => {
+//             let usersNotes;
+//             if (usersNotes = [].concat(JSON.parse(notes))) {
+//                 usersNotes = [];
+//             } else {
+//                 console.log("No notes found!");
+//             }
+//             // try {
+//             //     usersNotes === [].concat(JSON.parse(notes));
+//             // } catch (err) {
+//             //     usersNotes = [];
+//             // }
 
-            return usersNotes;
-        }); 
-    }
-    addNotes(note) {
-        const {title, text} = note;
-        if(!title || !text) {
-            console.log("Must add title and text to your note!");
-        }
-        const newNote = {title, text, id: uuid.v1()};
-        return this.getNotes().then((notes) => [...notes, newNote]).then((updatedNote) => this.write(updatedNote)).then(() => newNote);
-    }
-    removeNotes(id) {
-        return this.getNotes().then((notes) => notes.filter((note) => note.id !==id)).then((filteredNote) => this.write(filteredNote));
-    }
-}
+//             return usersNotes;
+//         }); 
+//     }
+//     addNotes(note) {
+//         const {title, text} = note;
+//         if(!title || !text) {
+//             console.log("Must add title and text to your note!");
+//         }
+//         const newNote = {title, text, id: uuid.v1()};
+//         return this.getNotes().then((notes) => [...notes, newNote]).then((updatedNote) => this.write(updatedNote)).then(() => newNote);
+//     }
+//     removeNotes(id) {
+//         return this.getNotes().then((notes) => notes.filter((note) => note.id !==id)).then((filteredNote) => this.write(filteredNote));
+//     }
+// }
 
-module.exports = new Notes();
+// module.exports = new Notes();
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,14 +1,39 @@
 const router = require("express").Router();
-const notes = require("../db/notes");
+const fs = require("fs");
+let dbNotes = require("../db/db.json");
 
-router.get("/notes", (req, res) => {
-    notes.getNotes().then((notes) => res.json(notes)).catch((err) => res.status(500).json(err));
+router.get("/api/notes", (req, res) => {
+    dbNotes = JSON.parse(fs.readFileSync("db/db.json"));
+    res.json(dbNotes);
 });
 router.post("/api/notes", (req, res) => {
-    notes.addNotes(req.body).then((note) => res.json(note)).catch((err) => res.status(500).json(err));
+    const usersNote = {
+        title: req.body.title,
+        text: req.body.text,
+        id: Math.floor(Math.random() * 99)
+    }
+    dbNotes.push(usersNote);
+    fs.writeFileSync("db/db.json", JSON.stringify(dbNotes), (err) => {
+        if (err) {
+            console.log(err);
+        }
+    });
+    res.json(dbNotes);
 });
 router.delete("/notes/:id", (req, res) => {
-    notes.removeNotes(req.params.id).then(() => res.json({ok: true})).catch((err) => res.status(500).json(err));
+    const newNotes = [];
+    for (let i = 0; i < dbNotes.length; i++) {
+        if (dbNotes[i] !=req.params.id) {
+            newNotes.push(dbNotes[i]);
+        }
+    }
+    dbNotes = newNotes;
+    fs.writeFileSync("db/db.json", JSON.stringify(dbNotes), (err) => {
+        if (err) {
+            console.log(err);
+        }
+    });
+    res.json(dbNotes);
 });
 
 module.exports = router;
